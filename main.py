29c4b65b614abd1b1deafa067b6eb2d1dceb826c
Rustam Madusmanov <accurat9@gmail.com>
2023-06-16 19:37:20 +0500

    изменил решение

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ n = int(input())
 m = int(input())
 k = int(input())
 
-if k < n + m and k % m == 0 or k % n == 0:
+if k < n + m and (k % m == 0 or k % n == 0):
     print("yes")
 else:
     print("no")
